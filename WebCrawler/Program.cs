fcaf07b3b923948188a94e36995972eef8a39c2d
eugene123tw <eugene123tw@gmail.com>
2016-11-08 16:30:53 +0800

    update HttpWebRequest

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace WebCrawler
 {
@@ -18,6 +20,15 @@ namespace WebCrawler
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
             Console.WriteLine("Hello World");
+            string url = "https://arachnode.net/media/g/releases/default.aspx";
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.UserAgent = "A .NET Web Crawler";
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string htmlText = reader.ReadToEnd();
+            Console.WriteLine(htmlText);
         }
     }
 }
